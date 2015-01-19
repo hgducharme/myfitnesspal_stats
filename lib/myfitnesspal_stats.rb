c7b3d107fded4b86f953da078439baf2f0cf8779
Hunter Ducharme <hgducharme@gmail.com>
2015-01-19 17:47:09 -0600

    added basic structure

--- lib/myfitnesspal_stats.rb
+++ lib/myfitnesspal_stats.rb
@@ -1,5 +1,30 @@
-require "myfitnesspal_stats/version"
+require_relative 'myfitnesspal_stats/version'
+require 'net/http'
+require 'open-uri'
+require 'yaml'
+require 'mechanize'
+
+# Possible gems to use:
+# - markaby for generating html
+# - RedCloth for generating html
+# - hpricot or searching within html documents
+# - REXML for parsing XML
 
 module MyfitnesspalStats
-  # Your code goes here...
+=begin
+  class Day
+
+  end
+
+  class Food
+
+  end
+
+  class Weight
+
+  end
+=end
 end
+
+account = MyfitnesspalStats::Account.new
+account.login
