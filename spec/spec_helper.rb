1df0789d614ab9e2342b4bdf11987ae4a685281d
Hunter Ducharme <hgducharme@gmail.com>
2015-01-20 17:38:07 -0600

    set FakeWeb net connect to false

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ require 'myfitnesspal_stats'
 # Libraries required for some test suites
 require 'mechanize'
 require 'fakeweb'
+FakeWeb.allow_net_connect = false
 
 RSpec.configure do |config|
   config.mock_with :rspec do |c|
