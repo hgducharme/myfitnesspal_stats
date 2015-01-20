a9e8b7ba10d553ed8c0d4ca47cfc9b80e372ab84
Hunter Ducharme <hgducharme@gmail.com>
2015-01-19 19:43:53 -0600

    added tests for #login

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,24 +1,33 @@
 require 'spec_helper'
 
 describe Account do
-  before :all do
+  before :each do
     @account = Account.new('bob', '1234')
   end
 
   describe '#initialize' do
     it 'takes a username and password argument successfully' do
-      @account.should be_an_instance_of Account
+      expect(@account).to be_an_instance_of(Account)
     end
   end # -- #initialize
 
   describe '#login' do
-    it 'connects to myfitnesspal homepage' do
-      stream = File.read('')
-      host = "https://www.myfitnesspal.com/"
-      web_crawler = Mechanize.new
-      home_page = web_crawler.get(host)
+    before :each do
+      stream = File.read('spec/html/mfp_home_page.html')
+      FakeWeb.register_uri(:get,
+        "https://www.myfitnesspal.com/",
+        :body => stream,
+        :content_type => "text/html")
+    end # -- before :each
 
-      expect(home_page.title).to be true
+    it 'clicks the log in link' do
+      web_crawler = Mechanize.new
+      home_page = web_crawler.get("https://www.myfitnesspal.com/")
+
+      # It returns a hash w/ attributes, so get the #text attribute
+      log_in_button = home_page.link_with(:href => /#fancy_login/).text
+
+      expect(log_in_button).to eq("#fancy_login")
     end
   end # -- #login
 
