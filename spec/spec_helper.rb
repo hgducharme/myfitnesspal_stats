f85f3e89138ff508c42508942578761d0ff835b6
Hunter Ducharme <hgducharme@gmail.com>
2015-01-22 23:04:30 -0600

    organized require code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
+require 'mechanize'
 require 'bundler/setup'
 Bundler.setup
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'myfitnesspal_stats'
 
-# Libraries required for some test suites
-require 'mechanize'
 
 RSpec.configure do |config|
   config.mock_with :rspec do |c|
