b3a059b4b57819a701c12f1673e24e0e8d51fecd
Hunter Ducharme <hgducharme@gmail.com>
2015-01-24 17:34:30 -0600

    moved vcr config to vcr_setup.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,10 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'myfitnesspal_stats'
 require 'bundler/setup'
 require 'webmock/rspec'
+require 'support/vcr_setup.rb'
 require 'net/http'
 require 'uri'
 require 'mechanize'
-require 'vcr'
 Bundler.setup
 
 RSpec.configure do |config|
@@ -17,10 +17,4 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = [:should, :expect]
   end
-end
-
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/vcr_cassettes"
-  config.hook_into :webmock
-  config.configure_rspec_metadata!
 end
