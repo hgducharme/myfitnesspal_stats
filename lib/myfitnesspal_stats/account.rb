6f0b8e14df3fd0ba3e9296929e4fb37bba7acb01
Hunter Ducharme <hgducharme@gmail.com>
2015-01-24 18:40:54 -0600

    Temporarily commented out all code

--- lib/myfitnesspal_stats/account.rb
+++ lib/myfitnesspal_stats/account.rb
@@ -12,14 +12,14 @@ class Account
       # MFP refreshes after login
       web_crawler.follow_meta_refresh = true
     end
-
+=begin
     # Go to homepage, click log in, and submit the form
     home_page = web_crawler.get('http://www.myfitnesspal.com/')
     login_btn = web_crawler.click(home_page.link_with(:href => /#fancy_login/))
 
     login_form = home_page.forms_with(:action => "https://www.myfitnesspal.com/account/login")
     login_form.each { |f| puts f.action }
-=begin
+
         # Complete username field
     login_form.input_with(:name => 'username') do |username| 
       username = @username
