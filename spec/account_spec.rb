127519aa574ad93603db8f4f06e8df1740016616
Hunter Ducharme <hgducharme@gmail.com>
2015-02-08 16:50:30 -0600

    refactored semantics

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,30 +1,26 @@
 require 'spec_helper'
 
-describe Account do
+describe Account, :vcr do
   before(:each) { @account = Account.new('bob', '1234') }
 
-  describe '#initialize' do
+  describe 'the initilization of an Account' do
     it 'takes a username and password argument successfully' do
       expect(@account).to be_an_instance_of(Account)
     end
-
-    it 'sets the username and password to an instance variable' do
-      @account.should respond_to(:username)
-      @account.should respond_to(:password)
-    end
   end
 
-  describe '#login' do
+  describe 'the process of logging into MFP' do
     let(:home_page) { "http://www.google.com" }
 
     # This is arbitrary, because Mechanize already tests the HTTP connection
-    it 'connects to the home page', :vcr do
-      home_page_cassette = File.new('vcr_cassettes/Account/_login/connects_to_the_home_page.yml', 'r')
-
+    # Skip because implementation of VCR & Webmock needs revision
+    skip 'connects to the home page' do
+      home_page_cassette = File.new(
+        'vcr_cassettes/Account/_login/connects_to_the_home_page.yml', 'r')
       stub_request(:get, home_page).to_return(home_page_cassette)
     end
 
-    it 'completes the login form and submits it', :vcr do
+    it 'completes the login form and submits it' do
 
     end
   end
