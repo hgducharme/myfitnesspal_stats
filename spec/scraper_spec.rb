ebb5feea06b43a98256f17608b98a261cbd2d73b
Hunter Ducharme <hgducharme@gmail.com>
2015-02-08 17:46:50 -0600

    cleaned up whitespace

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe Scraper, :vcr do 
-
  describe 'the initialization of a Scraper' do
     it 'takes a username and password argument successfully' do
       @scraper = Scraper.new('bob', '1234')
@@ -22,5 +21,4 @@ describe Scraper, :vcr do
 
     end
   end
-
 end
