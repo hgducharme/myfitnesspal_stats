f4ba8fa9c7ab2daf18b24aaac2158a3d1feaa36f
Hunter Ducharme <hgducharme@gmail.com>
2015-02-08 14:40:26 -0600

    added private methods: find_totals and invalid_date?

--- lib/myfitnesspal_stats/scraper.rb
+++ lib/myfitnesspal_stats/scraper.rb
@@ -13,18 +13,42 @@ class Scraper
     end
   end # .initialize
 
-  def get_macros
+  def get_macros(year = nil, month = nil, day = nil)
+    find_totals(year, month, day)
+  end
+
+  private
+
+  def find_totals(year, month, day)
+    #date = Date.new(year, month, day)
+
     diary = @login_page.link_with(href: "/food/diary/#{@username}").click
-    totals = diary.search('tr.total')
+
+    totals_table = diary.search('tr.total')
 
     # Find which nutrients are being tracked, and put them into an array
-    nutrient_titles = Hash.new
     nutrients = diary.search('tfoot').search('td.alt').text.split(
       /(?<=[a-z])(?=[A-Z])/).to_a
 
+    nutrient_totals = Hash.new
+    #unless invalid_date 
+     # nutrient_totals["Date"] = date.strftime("%A, %B, %e, %Y")
+   # end
+
     # For each nutrient, iterate through the table rows and get the value in its respective column. Then push the nutrient and value into a hash.
     nutrients.each_with_index do |nutrient, index|
-      puts totals.search('td')[index+1]
+      todays_total = totals_table.search('td')[index+1].text.strip
+      daily_goal = totals_table.search('td')[index+9].text.strip
+      remaining = totals_table.search('td')[index+17].text.strip
+      puts "-- #{nutrient}: \n Total: #{todays_total} \n Your goal: #{daily_goal} \n Remaining: #{remaining} \n\n"
+
+      nutrient_totals[nutrient] = todays_total, daily_goal, remaining
     end
+
+    pp nutrient_totals
+  end
+
+  def self.invalid_date?
+
   end
 end
