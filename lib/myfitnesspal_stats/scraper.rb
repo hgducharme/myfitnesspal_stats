faaf37ef000d872d743b6d26995e02886cdcc1f1
Hunter Ducharme <hgducharme@gmail.com>
2015-02-08 18:50:48 -0600

    finished get_nutrition method. returns totals for today

--- lib/myfitnesspal_stats/scraper.rb
+++ lib/myfitnesspal_stats/scraper.rb
@@ -1,29 +1,26 @@
 require_relative 'account'
+require_relative 'day'
 require 'mechanize'
 
 class Scraper
   def initialize(username, password)
     @username = username
     @password = password
+
+    @date = Date.today
+
     @account = Account.new(username, password)
+    @login_page = @account.login
 
     @web_crawler = Mechanize.new do |web_crawler|
-      @login_page = @account.login
       web_crawler.follow_meta_refresh = true
     end
   end # .initialize
 
-  def get_macros(year = nil, month = nil, day = nil)
-    find_totals(year, month, day)
-  end
-
-  private
-
-  def find_totals(year, month, day)
-    #date = Date.new(year, month, day)
+  def get_nutrition(year = @date.year, month = @date.month, day = @date.day)
+    date = Date.new(year, month, day)
 
     diary = @login_page.link_with(href: "/food/diary/#{@username}").click
-
     totals_table = diary.search('tr.total')
 
     # Find which nutrients are being tracked, and put them into an array
@@ -31,9 +28,7 @@ class Scraper
       /(?<=[a-z])(?=[A-Z])/).to_a
 
     nutrient_totals = Hash.new
-    #unless invalid_date 
-     # nutrient_totals["Date"] = date.strftime("%A, %B, %e, %Y")
-   # end
+    nutrient_totals["Date"] = date.strftime("%A, %B %e, %Y")
 
     # For each nutrient, iterate through the table rows and get the value in its respective column. Then push the nutrient and value into a hash.
     nutrients.each_with_index do |nutrient, index|
@@ -45,10 +40,7 @@ class Scraper
       nutrient_totals[nutrient] = todays_total, daily_goal, remaining
     end
 
-    pp nutrient_totals
+    nutrient_totals
   end
 
-  def self.invalid_date?
-
-  end
 end
