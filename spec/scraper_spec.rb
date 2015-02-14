e48147d69d88a24f768c38b314eec1a05760a365
Hunter Ducharme <hgducharme@gmail.com>
2015-02-14 10:56:59 -0600

    added dummy login

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -1,16 +1,17 @@
 require 'spec_helper'
 
 describe Scraper, :vcr do 
+  
  describe 'the initialization of a Scraper' do
     it 'takes a username and password argument successfully' do
-      @scraper = Scraper.new('bob', '1234')
+      @scraper = Scraper.new('rspec_test', '123456')
       expect(@scraper).to be_an_instance_of(Scraper)
     end
   end
 
-  describe 'the get_macros method' do
+  describe 'the get_nutrition method' do
     it 'takes a date' do
-      
+
     end
 
     it 'does not fail if no date is given' do
