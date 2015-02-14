d841e67c2016e1abc027cf429a3f8a88214019f9
Hunter Ducharme <hgducharme@gmail.com>
2015-02-14 12:19:52 -0600

    moved get_nutrition to Day class, and added get_date method

--- lib/myfitnesspal_stats/scraper.rb
+++ lib/myfitnesspal_stats/scraper.rb
@@ -16,33 +16,9 @@ class Scraper
       web_crawler.cookie_jar.load('cookies.yml')
       web_crawler.follow_meta_refresh = true
     end
-  end # .initialize
+  end # ---- initialize
 
-  def get_nutrition(year = @date.year, month = @date.month, day = @date.day)
-    date = Date.new(year, month, day)
-
-    # @login_page.uri already has a trailing slash: /
-    diary = @web_crawler.get("#{@login_page.uri}food/diary/#{@username}?date=#{date}")
-    totals_table = diary.search('tr.total')
-
-    # Find which nutrients are being tracked, and put them into an array
-    nutrients = diary.search('tfoot').search('td.alt').text.split(
-      /(?<=[a-z])(?=[A-Z])/).to_a
-
-    nutrient_totals = Hash.new
-    nutrient_totals["Date"] = date.strftime("%A, %B %e, %Y")
-
-    # For each nutrient, iterate through the table rows and get the value in its respective column. Then push the nutrient and value into a hash.
-    nutrients.each_with_index do |nutrient, index|
-      todays_total = totals_table.search('td')[index+1].text.strip
-      daily_goal = totals_table.search('td')[index+9].text.strip
-      remaining = totals_table.search('td')[index+17].text.strip
-      #puts "-- #{nutrient}: \n Total: #{todays_total} \n Your goal: #{daily_goal} \n Remaining: #{remaining} \n\n"
-
-      nutrient_totals[nutrient] = todays_total, daily_goal, remaining
-    end
-
-    nutrient_totals
-  end
-
+  def get_date(year = @date.year, month = @date.month, day = @date.day)
+    day = Day.new(year, month, day)
+  end # ---- get_date
 end
