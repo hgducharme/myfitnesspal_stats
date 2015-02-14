278e9ec1d0a9e9f073ba6cd0090ceb19197549ac
Hunter Ducharme <hgducharme@gmail.com>
2015-02-13 19:05:33 -0600

    added login_cookies functionality

--- lib/myfitnesspal_stats/account.rb
+++ lib/myfitnesspal_stats/account.rb
@@ -5,23 +5,23 @@ class Account
   def initialize(username = nil, password = nil)
     @username = username
     @password = password
+
+    @web_crawler = Mechanize.new do |web_crawler|
+      web_crawler.follow_meta_refresh = true
+    end
   end
 
   def login
-    web_crawler = Mechanize.new do |web_crawler|
-      # MFP refreshes after login
-      web_crawler.follow_meta_refresh = true
-    end
-
     # Go to homepage, click log in, and submit the form
-    home_page = web_crawler.get('http://www.myfitnesspal.com/')
+    home_page = @web_crawler.get('http://www.myfitnesspal.com/')
     login_form = home_page.form_with(id: "fancy_login")
     login_form['username'] = @username
     login_form['password'] = @password
     current_page = login_form.submit
+    login_cookies = @web_crawler.cookie_jar.save('cookies.yml', session: true)
 
     # Checks to see if there was an error when logging in
-   begin
+    begin
       calories_left = current_page.search('div#calories-remaining-number').text
       puts "#{@username} has successfully logged in!"
       return current_page
