461383ab98eaf963998b9048c89f16f9b3aa38f2
Hunter Ducharme <hgducharme@gmail.com>
2015-02-26 23:15:06 -0600

    basic initialization

--- lib/myfitnesspal_stats/meal.rb
+++ lib/myfitnesspal_stats/meal.rb
@@ -5,9 +5,6 @@ class Meal
     $web_crawler = Mechanize.new do |web_crawler|
       web_crawler.cookie_jar.load('cookies.yml')
       web_crawler.follow_meta_refresh = true
-
-      login_page = 'http://www.myfitnesspal.com'
-      $food_diary = web_crawler.get("#{login_page}/food/diary/#{@username}?date=#{@date}")
     end
   end # ---- initialize
-end
+end # ---- class Meal
